fe25b023548cd68fb93b1481d5f224a4f3730b9f
commonkestrel <jettb6@gmail.com>
2024-01-08 08:08:30 -0800

    Add PathPlanner auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -15,25 +18,25 @@ import frc.robot.subsystems.Swerve;
 public class RobotContainer {
     private final CommandXboxController m_controller = new CommandXboxController(IOConstants.controllerPort);
 
-    private static final Swerve m_drive = Swerve.getInstance();
+    private static final Swerve m_swerve = Swerve.getInstance();
     private static final Conveyor m_conveyor = Conveyor.getInstance();
 
     public RobotContainer() {
         configureBindings();
 
-        m_drive.setDefaultCommand(Commands.run(() -> {
+        m_swerve.setDefaultCommand(Commands.run(() -> {
             double forward = MathUtil.applyDeadband(m_controller.getLeftY(), IOConstants.transDeadband);
             double strafe = MathUtil.applyDeadband(m_controller.getLeftX(), IOConstants.transDeadband);
             double rotation = MathUtil.applyDeadband(m_controller.getRightX(), IOConstants.rotDeadband);
             double speed = m_controller.getRightTriggerAxis();
 
-            m_drive.drive(
+            m_swerve.drive(
                 forward * speed * (IOConstants.xyInverted ? -1.0 : 1.0),
                 strafe * speed * (IOConstants.xyInverted ? -1.0 : 1.0),
                 rotation * speed * (IOConstants.rotInverted ? -1.0 : 1.0),
                 true, true
             );
-        }, m_drive));
+        }, m_swerve));
 
         m_conveyor.setDefaultCommand(Commands.run(() -> {
             if (m_conveyor.noteDetected()) {
@@ -46,9 +49,11 @@ public class RobotContainer {
 
     private void configureBindings() {
         m_controller.rightBumper().onTrue(m_conveyor.advance());
+
+        NamedCommands.registerCommand("score", m_conveyor.waitForNote().andThen(m_conveyor.advance()));
     }
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        return new PathPlannerAuto("Two-piece Auto");
     }
 }
