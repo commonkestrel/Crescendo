c374c9a0d37a39c32e0fcdd02064c39ef4608f09
commonkestrel <jettb6@gmail.com>
2024-01-08 21:36:08 -0800

    update smartdashboard

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -78,5 +78,6 @@ public class Conveyor extends SubsystemBase {
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Conveyor Distance", m_drive.getPosition());
+        SmartDashboard.putBoolean("Note Ready", noteDetected());
     }
 }
