00ce42d7b1c23b751e1713e4e296a088d5d0f8e2
Wild Raccoons <jettb6@gmail.com>
2024-02-22 18:16:33 -0800

    Add actual robot finally?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,14 +14,19 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.IOConstants;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.drive.Swerve;
+import wildlib.Toggle;
 
 public class RobotContainer {
     private final CommandXboxController m_controller = new CommandXboxController(IOConstants.controllerPort);
 
     private static final Swerve m_swerve = Swerve.getInstance();
     private static final Intake m_intake = Intake.getInstance();
+    private static final Shooter m_shooter = Shooter.getInstance();
     private static final Limelight m_limelight = Limelight.getInstance();
+
+    private static Toggle m_shootingSpeaker = new Toggle(false);
 
     public RobotContainer() {
         configureBindings();
@@ -39,23 +44,34 @@ public class RobotContainer {
                 true, true
             );
         }, m_swerve));
-
-        m_intake.setDefaultCommand(Commands.run(() -> {
-            if (m_intake.noteDetected()) {
-                m_intake.stop();
-            } else {
-                m_intake.setSpeed(0.8);
-            }
-        }, m_intake));
     }
 
     private void configureBindings() {
-        m_controller.rightBumper().onTrue(m_intake.advance());
+        m_controller.rightBumper().onTrue(Commands.either(shootAmp(), shootSpeaker(), m_shootingSpeaker));
+        m_controller.a().onTrue(m_shootingSpeaker.setFalse());
+        m_controller.y().onTrue(m_shootingSpeaker.setTrue());
 
-        NamedCommands.registerCommand("score", m_intake.waitForNote().andThen(m_intake.advance()));
+        NamedCommands.registerCommand("score", shootAmp());
+    }
+
+    private Command shootAmp() {
+        return m_intake.waitForNote()
+            .andThen(
+                m_shooter.rampAmp(), 
+                m_intake.advanceAmp().alongWith(m_shooter.waitForShoot())
+            );
+    }
+
+    private Command shootSpeaker() {
+        return m_intake.waitForNote()
+            .andThen(
+                m_shooter.rampSpeaker(), 
+                m_intake.advanceSpeaker().alongWith(m_shooter.waitForShoot())
+            );
     }
 
     public Command getAutonomousCommand() {
         return new PathPlannerAuto("Two-piece Auto");
     }
 }
+// :)
