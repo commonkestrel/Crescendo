bec1969342c4ac7fb6b4049a5aeec99d0bd46dae
Wild Raccoons <jettb6@gmail.com>
2024-04-13 21:20:14 -0700

    optimise timing

--- src/main/java/frc/robot/commands/ShootSpeakerCommand.java
+++ src/main/java/frc/robot/commands/ShootSpeakerCommand.java
@@ -76,6 +76,6 @@ public class ShootSpeakerCommand extends Command {
     @Override
     public boolean isFinished() {
         // Wait one second after the note is last detected before ending
-        return m_currentState == State.Wait && System.nanoTime() - m_lastDetected >= 1e9;
+        return m_currentState == State.Wait && System.nanoTime() - m_lastDetected >= 0.25e9;
     }
 }
